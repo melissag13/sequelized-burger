9ff271f670bdd17f44ec8c4b6fdace0c372e2d0a
Melissa Garcia <melissag13@gmail.com>
2016-10-15 21:21:16 -0400

    trying to fix

--- config/connection.js
+++ config/connection.js
@@ -5,10 +5,10 @@ if (process.env.JAWSDB_URL) {
 	connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
-    host: 'localhost',
+    host: 'localhost3',
     user: 'root',
     password: '',
-    database: 'burgers_db'
+    database: 'sequelized_burgers_db'
   });
 };
 
